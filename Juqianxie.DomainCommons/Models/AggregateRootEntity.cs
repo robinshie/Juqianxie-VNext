21efb35953b662c59a98910fd39c9a19821f3bf3
Robin <robin.shie@outlook.com>
2023-04-24 11:36:52 +0800

    add Base Model Aggregate

---
+++ Juqianxie.DomainCommons/Models/AggregateRootEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juqianxie.DomainCommons.Models
+{
+    public record AggregateRootEntity : BaseEntity, IAggregateRoot, ISoftDelete, IHasCreationTime, IHasDeletionTime, IHasModificationTime
+    {
+        public bool IsDeleted { get; private set; }
+        public DateTime CreationTime { get; private set; } = DateTime.Now;
+        public DateTime? DeletionTime { get; private set; }
+        public DateTime? LastModificationTime { get; private set; }
+
+        public void SoftDelete()
+        {
+            this.IsDeleted = true;
+            this.DeletionTime = DateTime.Now;
+        }
+        public void NotifyModified()
+        {
+            this.LastModificationTime = DateTime.Now;
+        }
+    }
+}
---
+++ Juqianxie.DomainCommons/Models/ISoftDelete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juqianxie.DomainCommons.Models
+{
+    public interface ISoftDelete
+    {
+        bool IsDeleted { get; }//不能写成get;protected set;否则在实现类中，这个属性不能是public http://www.itpow.com/c/2019/05/11443.asp
+        void SoftDelete();
+    }
+}
