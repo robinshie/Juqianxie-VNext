7b47d3d7e1d26ca6c99b1e9ba776af654262d2c0
Robin <robin.shie@outlook.com>
2023-08-20 18:39:41 +0200

    add struckt

--- IdentityService.Infrastructure/Migrations/20230820141743_init.Designer.cs
+++ IdentityService.Infrastructure/Migrations/20230820141743_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IdentityService.Infrastructure.Migrations
 {
     [DbContext(typeof(IdDbContext))]
-    [Migration("20230820133030_init")]
+    [Migration("20230820141743_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
